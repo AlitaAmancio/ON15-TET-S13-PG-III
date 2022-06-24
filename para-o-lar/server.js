40528bc491a7f50304d0ed5a92ade2196151c06f
AlitaAmancio <amancioalita@gmail.com>
2022-06-24 14:14:56 -0300

    subindo atualizações

--- para-o-lar/server.js
+++ para-o-lar/server.js
@@ -1,4 +1,5 @@
 const app = require('./src/app');
 const PORT = process.env.PORT;
+// servem como portas disponíveis 3000, 3030, 6000, 6060, 8000, 8080
 
 app.listen(PORT, () => console.log(`Servidor em funcionamento na porta ${PORT}`));
